d9164acaf43c4fdcbc9c9bc40010980c1943f9d7
AshishMittal345 <163109086+AshishMittal345@users.noreply.github.com>
2026-01-16 13:55:12 +0530

    commit

--- src/vectorize_script.py
+++ src/vectorize_script.py
@@ -4,7 +4,7 @@ from vectorize_book import vectorize_book_and_store_to_db, vectorize_chapters
 
 load_dotenv()
 
-CLASS_SUBJECT_NAME = os.getenv("CLASS_SUBJECT_NAME")
+CLASS_SUBJECT_NAME = os.getenv("CLASS_BASE_PATH")
 
 vectorize_book_and_store_to_db(CLASS_SUBJECT_NAME, "book_vector_db")
 vectorize_chapters(CLASS_SUBJECT_NAME)
